e36faf3061d6adc635c8eb402277127a2b7890f6
Ahror <kayumov.ahror@gmail.com>
2019-01-29 15:24:07 +0500

    removed unused namespaces from model

--- Connector/Abstractions/BaseEntity.cs
+++ Connector/Abstractions/BaseEntity.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Connector.Abstractions
+﻿namespace Connector.Abstractions
 {
     /// <summary>
     /// Base class for trade and candle 
